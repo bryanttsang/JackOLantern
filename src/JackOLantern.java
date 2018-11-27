e586c6254f33155e8855492a1102c6705307ec6f
BT_1N3_21 <wtsang1931@bths.edu>
2018-11-27 14:47:55 -0500

    11/27

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -14,12 +14,29 @@ public class JackOLantern
 
     public void fill(String str)
     {
-
+        for (int i = 0; i < faceFeatures.length; i++)
+        {
+            for (int ii = 0; ii < faceFeatures[i].length; ii++)
+            {
+                faceFeatures[i][ii] = str;
+            }
+        }
     }
 
     @Override
     public String toString()
     {
-        
+        String toString = "";
+        for (int i = 0; i < faceFeatures.length; i++)
+        {
+            for (int ii = 0; ii < faceFeatures[i].length; ii++)
+            {
+                toString = toString + faceFeatures[i][ii];
+                if (ii == faceFeatures[i].length) {
+                    toString = toString + "\n";
+                }
+            }
+        }
+        return toString;
     }
 }
